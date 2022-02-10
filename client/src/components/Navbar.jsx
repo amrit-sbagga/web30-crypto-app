c9a25c733ba0dcb1b6e0acaf217b2ec1c13b9653
Amrit Pal Singh <amrit.s@proximity.tech>
2022-02-11 00:59:51 +0530

    Modified navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/ai';
-//import logo from '../../images/logo.png';
+import React, { Component } from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../images/logo.png";
 
 export class Navbar extends Component {
   render() {
-    return <div>Navbar</div>;
+    return (
+      <nav className="flex w-full items-center justify-between p-4 md:justify-center">
+        Navbar
+      </nav>
+    );
   }
 }
 
