2c8750924d85e693228ee84dac60998ffc4bc3cb
Amrit Pal Singh <amrit.s@proximity.tech>
2022-02-11 00:49:48 +0530

    Added react icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+//import logo from '../../images/logo.png';
 
 export class Navbar extends Component {
   render() {
